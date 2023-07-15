e3404d8e23a491424a1b9b31e32506d8941097fa
SwapnilJambhulkar <swapniljambhulkar70@gmail.com>
2023-07-15 10:20:30 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '32405b87-ee0e-43f9-9507-9ec9a7ac1b6d';
+export const MODAL_CLOSE_SEC = 2.5;
